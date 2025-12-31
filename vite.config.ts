ebfce8cdffac0729feb943ce251ffd0ad54dc9cf
Karlos Montoya <karlosmontoya91@gmail.com>
2025-12-31 13:31:37 -0600

    Fix: set Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
 export default defineConfig({
+  base: '/portafolio_karlos_con_k_UXUI/',   // 👈 AQUÍ repo en github
   plugins: [react()],
   resolve: {
     alias: {
